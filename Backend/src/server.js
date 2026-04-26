56e5028e0c8d28c11c32c1143d26b2f81312d707
PrasoonKumar9759 <pirsoonkumar@gmail.com>
2026-04-26 23:13:59 +0530

    ready to deploy

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -39,8 +39,8 @@ app.use("/api/notes", notesRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
